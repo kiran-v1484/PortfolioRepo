5f762e300b2ebfa3b0142434e437fd7870fcd7d9
kiran-v1484 <kiran.vethekar@gmail.com>
2024-09-17 15:58:01 +0530

    Canvas component splitted separate and routes added for navigation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,25 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
+
 import "./style.css";
-import { OrbitControls, useTexture } from "@react-three/drei";
-import { MeshStandardMaterial } from "three";
-import * as THREE from "three";
-import Cyl from "./Cyl";
-import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
+
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Canvas3d from "./components/Canvas3d";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
 
 const App = () => {
-  return (<>
-    <Canvas flat camera={{ fov: 35 }}>
-      {/* <OrbitControls /> */}
-      <ambientLight />
-      <Cyl />
-      <EffectComposer>
-        <Bloom
-          mipmapBlur
-          intensity={4.0}
-          luminanceThreshold={0}
-          luminanceSmoothing={0}
-        />
-        <ToneMapping adaptive/>
-      </EffectComposer>
-    </Canvas>
-    <div className="desc">
-      this is my portfolio
-    </div>
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route index element={<Canvas3d />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="resume" element={<Resume />} />
+        <Route path="Projects" element={<Projects />} />
+      </Routes>
     </>
   );
 };
