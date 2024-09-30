c10e223ab4ba61997a2194fbf10161cc58013cd6
kiran-v1484 <kiran.vethekar@gmail.com>
2024-09-30 13:35:51 +0530

    Resume image added to the component

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Resume = () => {
+
+  const downloadFile = () => {
+    window.location.href = "./Kiran-Resume-pdf.pdf"
+  }
   return (
     <div>
-      Resume Component rendering 
-      
+       <div className="button-div"><button id="blue-btn" onClick={downloadFile}>download PDF file</button></div>
+      <div className="resume-div">
+        <img src="./Kiran-Resume.jpg" alt="Kiran-Resume" />
+      </div>
+     
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
